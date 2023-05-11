2d8eed5026b53d613512e90c4c39b7fbd9f769ff
Stanislav Pankevich <s.pankevich@gmail.com>
2023-05-11 22:22:08 +0100

    builders/minimal_builder: in-memory reading and writing

--- builders/minimal_builder.py
+++ builders/minimal_builder.py
@@ -1,23 +1,40 @@
+import codecs
 import logging
-from typing import Any, Iterable, Optional, Sequence
+import time
+from os import path
+from typing import Any, Iterable, Optional, Sequence, List
 
+import docutils
 from docutils import nodes
+from docutils.core import Publisher
 from docutils.frontend import OptionParser
-from docutils.io import StringOutput
+from docutils.io import StringOutput, NullOutput
+from docutils.utils import DependencyList
 from sphinx.application import Sphinx
+from sphinx.builders.html import StandaloneHTMLBuilder
 from sphinx.builders.singlehtml import SingleFileHTMLBuilder
 from sphinx.environment import BuildEnvironment
-from sphinx.util import relative_uri
+from sphinx.io import SphinxStandaloneReader, SphinxDummyWriter, SphinxFileInput
+from sphinx.util import relative_uri, rst, get_filetype, UnicodeDecodeErrorHandler
 from sphinx.util.console import darkgreen  # type: ignore
+from sphinx.util.docutils import sphinx_domains
 from sphinx.util.nodes import inline_all_toctrees
 from sphinx.util.parallel import SerialTasks
 from sphinx.writers.html import HTMLWriter
 
-# Force Sphinx to produce no logs.
-logging.disable(logging.CRITICAL)
+
+class MyRSTInputReader:
+    def __init__(self, input_rst):
+        self.input_rst = input_rst
+
+    def read(self):
+        return self.input_rst
+
+    def close(self):
+        pass
 
 
-class MinimalBuilder(SingleFileHTMLBuilder):
+class MinimalBuilder(StandaloneHTMLBuilder):
     name = 'minimal'
     format = 'custom'
 
@@ -28,29 +45,75 @@ class MinimalBuilder(SingleFileHTMLBuilder):
         self.doctree = None
         self.indexer = None
         self.output = None
+        self.strictdoc_input = None
         self.strictdoc_output = None
 
     def build(
         self, docnames: Iterable[str], summary: Optional[str] = None, method: str = 'update'
     ) -> None:
-        updated_docnames = set(self.read())
-
-        if updated_docnames:
-            # global actions
-            self.env.check_consistency()
-
-        self.parallel_ok = False
-
-        self.finish_tasks = SerialTasks()
+        self.read()
 
         # write all "normal" documents (or everything for some builders)
-        self.write(docnames, list(updated_docnames), method)
+        self.write(docnames, list(), method)
 
-        # finish (write static files etc.)
-        self.finish()
+    def read(self) -> List[str]:
+        """(Re-)read all files new or changed since last update.
+        Store all environment docnames in the canonical format (ie using SEP as
+        a separator in place of os.path.sep).
+        """
 
-        # wait for all tasks
-        self.finish_tasks.join()
+        self._read_serial([])
+
+        return []
+
+    def _read_serial(self, docnames: List[str]) -> None:
+        self.read_doc("index")
+
+    @staticmethod
+    def create_publisher(app: "Sphinx", filetype: str) -> Publisher:
+        reader = SphinxStandaloneReader()
+        reader.setup(app)
+
+        parser = app.registry.create_source_parser(app, filetype)
+
+        pub = Publisher(
+            reader=reader,
+            parser=parser,
+            writer=SphinxDummyWriter(),
+            source_class=SphinxFileInput,
+            destination=NullOutput()
+        )
+        # Propagate exceptions by default when used programmatically:
+        defaults = {"traceback": True, **app.env.settings}
+        # Set default settings
+        pub.get_settings(**defaults)  # type: ignore[arg-type]
+        return pub
+
+    def read_doc(self, docname: str) -> None:
+        # super().read_doc(docname)
+
+        """Parse a file and add/update inventory entries for the doctree."""
+        self.env.prepare_settings(docname)
+
+        filename = self.env.doc2path(docname)
+        publisher = MinimalBuilder.create_publisher(self.app, "restructuredtext")
+
+        with sphinx_domains(self.env), rst.default_role(docname, self.config.default_role):
+            # set up error_handler for the target document
+            codecs.register_error('sphinx', UnicodeDecodeErrorHandler(docname))  # type: ignore
+
+            my_rst_input_reader = MyRSTInputReader(self.strictdoc_input)
+            publisher.set_source(
+                source=my_rst_input_reader, source_path=filename
+            )
+            publisher.publish()
+            doctree = publisher.document
+
+        # cleanup
+        self.env.temp_data.clear()
+        self.env.ref_context.clear()
+
+        self.write_doctree(docname, doctree)
 
     def write(self, build_docnames: Iterable[str], updated_docnames: Sequence[str], method: str = 'update') -> None:  # NOQA
         # super().write(build_docnames, updated_docnames, method)
@@ -62,14 +125,14 @@ class MinimalBuilder(SingleFileHTMLBuilder):
         # doctree = self.assemble_doctree()
         master = self.config.root_doc
         tree = self.doctree
-        tree = inline_all_toctrees(self, set(), master, tree, darkgreen, [master])
+        # tree = inline_all_toctrees(self, set(), master, tree, darkgreen, [master])
         tree['docname'] = master
         self.env.resolve_references(tree, master, self)
-        self.fix_refuris(tree)
+        # self.fix_refuris(tree)
         doctree = tree
 
-        self.env.toc_secnumbers = self.assemble_toc_secnumbers()
-        self.env.toc_fignumbers = self.assemble_toc_fignumbers()
+        # self.env.toc_secnumbers = self.assemble_toc_secnumbers()
+        # self.env.toc_fignumbers = self.assemble_toc_fignumbers()
 
         # self.write_doc_serialized(self.config.root_doc, doctree)
         self.write_doc(self.config.root_doc, doctree)
@@ -102,10 +165,10 @@ class MinimalBuilder(SingleFileHTMLBuilder):
         destination = StringOutput(encoding='utf-8')
         doctree.settings = self.docsettings
         #
-        self.secnumbers = self.env.toc_secnumbers.get(docname, {})
-        self.fignumbers = self.env.toc_fignumbers.get(docname, {})
-        self.imgpath = relative_uri(self.get_target_uri(docname), '_images')
-        self.dlpath = relative_uri(self.get_target_uri(docname), '_downloads')
+        # self.secnumbers = self.env.toc_secnumbers.get(docname, {})
+        # self.fignumbers = self.env.toc_fignumbers.get(docname, {})
+        # self.imgpath = relative_uri(self.get_target_uri(docname), '_images')
+        # self.dlpath = relative_uri(self.get_target_uri(docname), '_downloads')
         self.current_docname = docname
         #
         self.docwriter.write(doctree, destination)
